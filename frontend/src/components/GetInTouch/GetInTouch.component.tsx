26cdfbd530c63df890f6559bfe62ed1f57767767
tappiola <karamorina@gmail.com>
2022-03-12 17:14:59 +0000

    style home page

---
+++ frontend/src/components/GetInTouch/GetInTouch.component.tsx
@@ -0,0 +1,28 @@
+import MessageIcon from '../MessageIcon';
+import PhoneIcon from '../PhoneIcon';
+import './GetInTouch.style.scss';
+
+const EMAIL = 'enquery@tappiola-agency.com';
+const PHONE = '+44 7507 625 808';
+
+const GetInTouch = () => (
+  <div className="GetInTouch">
+    <span className="GetInTouch-Title">Get in touch</span>
+    <span className="GetInTouch-Contact">
+      <MessageIcon />
+      <span className="GetInTouch-Label">Email us:</span>
+      <a href={`mailto:${EMAIL}`} className="GetInTouch-ContactLink">
+        {EMAIL}
+      </a>
+    </span>
+    <span className="GetInTouch-Contact">
+      <PhoneIcon />
+      <span className="GetInTouch-Label">Call us:</span>
+      <a href={`tel:${PHONE}`} className="GetInTouch-ContactLink">
+        {PHONE}
+      </a>
+    </span>
+  </div>
+);
+
+export default GetInTouch;
---
+++ frontend/src/components/PhoneIcon/PhoneIcon.component.tsx
@@ -0,0 +1,16 @@
+const PhoneIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="PhoneIcon"
+  >
+    <path
+      d="M9.37 4.907l1.735-.587a1 1 0 011.267.627l.962 2.842a1 1 0 01-.627 1.268l-1.895.64.004.83a7 7 0 001.418 4.193l.5.661 1.895-.64a1 1 0 011.268.626l.96 2.842a1 1 0 01-.626 1.268l-1.734.586a2 2 0 01-1.978-.407l-.518-.466A13 13 0 017.917 7.116l.13-.685A2 2 0 019.37 4.907z"
+      stroke="#fff"
+    />
+  </svg>
+);
+
+export default PhoneIcon;
